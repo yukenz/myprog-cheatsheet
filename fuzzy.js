07fbd42259630417dea3eb6c45e5b3be6ebbaa4b
yukenz <34228964+yukenz@users.noreply.github.com>
2023-08-24 10:53:23 +0700

    Update fuzzy.js (#1)

--- fuzzy.js
+++ fuzzy.js
@@ -3,6 +3,6 @@ const fuzzy = require('fuzzy');
 
 var list = ['baconing', 'narwhal', 'a mighty bear canoe'];
 var results = fuzzy.filter('oe', list)
-var matches = results.map(function (el) { return el.string; });
+var matches = results.map(function (x) { return x.string; });
 console.log(matches);
 // [ 'baconing', 'a mighty bear canoe' ]
